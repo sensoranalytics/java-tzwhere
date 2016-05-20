f3ee5c96cfc4fc1b12464444181b94d10ffe896e
Daniel Barclay <dbarclay@savi.com>
2016-05-20 13:13:15 -0700

    Added some test cases, cleaned.

--- src/test/java/com/savi/timezones/TzWhereTest.java
+++ src/test/java/com/savi/timezones/TzWhereTest.java
@@ -1,17 +1,11 @@
 package com.savi.timezones;
 
-import com.vividsolutions.jts.geom.Coordinate;
 import org.junit.*;
-
 import static org.hamcrest.CoreMatchers.*;
 import static org.junit.Assert.*;
 
 
-import java.io.IOException;
-
-
 public class TzWhereTest {
-
 
     @Test
     public void testSomePointGetsItsTimeZoneId() {
@@ -35,92 +29,21 @@ public class TzWhereTest {
         final String expected = "America/Indiana/Tell_City";
     }
 
-
     @Test
-    public void testXxxx10() {
-        final double latitude  = 40;
-        final double longitude = 10;
-        final String actual = TzWhere.getLatLogTimeZoneId(latitude, longitude);
-        final String expected = null; //??? CHECK
-
-        assertThat(actual, equalTo(expected));
-    }
-    @Test
-    public void testXxxx9() {
-        final double latitude  = 41;
-        final double longitude = 12;
-        final String actual = TzWhere.getLatLogTimeZoneId(latitude, longitude);
-        final String expected = null; //??? CHECK
-
-        assertThat(actual, equalTo(expected));
-    }
-    @Test
-    public void testXxxx8() {
-        final double latitude  = 41.9;
-        final double longitude = 12.4;
-        final String actual = TzWhere.getLatLogTimeZoneId(latitude, longitude);
-        final String expected = "Europe/Rome"; //??? CHECK
-
-        assertThat(actual, equalTo(expected));
-    }
-    @Test
-    public void testXxxx7() {
-        final double latitude  = 41.90;
-        final double longitude = 12.45;
-        final String actual = TzWhere.getLatLogTimeZoneId(latitude, longitude);
-        final String expected = "Europe/Rome"; //??? CHECK
-
-        assertThat(actual, equalTo(expected));
-    }
-    @Test
-    public void testXxxx6() {
-        final double latitude  = 41.906;
-        final double longitude = 12.450;
-        final String actual = TzWhere.getLatLogTimeZoneId(latitude, longitude);
-        final String expected = "Europe/Vatican"; //??? CHECK
-
-        assertThat(actual, equalTo(expected));
-    }
-    @Test
-    public void testXxxx5() {
-        final double latitude  = 41.90636;
-        final double longitude = 12.45091;
-        final String actual = TzWhere.getLatLogTimeZoneId(latitude, longitude);
-        final String expected = "Europe/Vatican"; //??? CHECK
-
-        assertThat(actual, equalTo(expected));
-    }
-    @Test
-    public void testXxxx4() {
-        final double latitude  = 41.9063606;
-        final double longitude = 12.4509162;
-        final String actual = TzWhere.getLatLogTimeZoneId(latitude, longitude);
-        final String expected = "Europe/Vatican"; //??? CHECK
-
-        assertThat(actual, equalTo(expected));
-    }
-    @Test
-    public void testXxxx3() {
-        final double latitude  = 41.906360626;
-        final double longitude = 12.450916290;
-        final String actual = TzWhere.getLatLogTimeZoneId(latitude, longitude);
-        final String expected = "Europe/Vatican"; //??? CHECK
-
-        assertThat(actual, equalTo(expected));
-    }
-    @Test
-    public void testXxxx2() {
-        final double latitude  = 41.90636062622;
-        final double longitude = 12.45091629028;
-        final String actual = TzWhere.getLatLogTimeZoneId(latitude, longitude);
-        final String expected = "Europe/Vatican"; //??? CHECK
-
-        assertThat(actual, equalTo(expected));
-    }
-    @Test
-    public void testXxxx1() {
+    public void testPointAtBoundaryVertex() {
+        // A point that is vertex on boundary polygon of Europe/Vatican:
         final double latitude  = 41.9063606262207;
-        final double longitude = 12.4509162902832;
+        final double longitude = 12.45091629028328;
+        final String actual = TzWhere.getLatLogTimeZoneId(latitude, longitude);
+        final String expected = "Europe/Vatican";
+
+        assertThat(actual, equalTo(expected));
+    }
+    @Test
+    public void testPointNearBoundaryVertex1() {
+        // A point that is just inside vertex on boundary polygon of Europe/Vatican:
+        final double latitude  = 41.9063606262207 - 0.002; // on northern part
+        final double longitude = 12.45091629028328;
         final String actual = TzWhere.getLatLogTimeZoneId(latitude, longitude);
         final String expected = "Europe/Vatican";
 
@@ -128,61 +51,75 @@ public class TzWhereTest {
     }
 
     @Test
-    public void testPointAtBoundaryVertex() {
-        // A point that is vertex on boundary polygon of Europe/Vatican:
-        final double latitude  = 41.9063606262207;
-        final double longitude = 12.4509162902832;
-        final String actual = TzWhere.getLatLogTimeZoneId(latitude, longitude);
-        final String expected = "Europe/Vatican"; //??? CHECK
-
-        assertThat(actual, equalTo(expected));
-    }
-    @Test
-    public void testPointNearBoundaryVertex1() {
-        // A point that is just inside vertex on boundary polygon of Europe/Vatican:
-        final double latitude  = 41.9063606262207 - 1e-6;
-        final double longitude = 12.4509162902832;
-        final String actual = TzWhere.getLatLogTimeZoneId(latitude, longitude);
-        final String expected = "Europe/Vatican"; //??? CHECK
-
-        assertThat(actual, equalTo(expected));
-    }
-
-    @Test
     public void testPointNearBoundaryVertex2() {
-        // A point that is just inside vertex on boundary polygon of Europe/Vatican:
-        final double latitude  = 41.9063606262207 + 1e-6;
-        final double longitude = 12.4509162902832;
+        // A point that is just outside vertex on boundary polygon of Europe/Vatican:
+        final double latitude  = 41.9063606262207 + 0.002; // on northern part
+        final double longitude = 12.45091629028328;
         final String actual = TzWhere.getLatLogTimeZoneId(latitude, longitude);
-        final String expected = "Europe/Vatican"; //??? CHECK
+        final String expected = "Europe/Rome";
 
         assertThat(actual, equalTo(expected));
     }
 
-
     @Test
-    public void testx1() {
-        final String zoneId = TzWhere.getLatLogTimeZoneId(90, 0);
-        System.err.println("zoneId = " + zoneId);
+    public void testMidoceanTimeZonesNotSupportedYet() {
+        final double latitude  = 0;
+        final double longitude = 0;
+        final String actual = TzWhere.getLatLogTimeZoneId(latitude, longitude);
+        final String expected = null;
+
+        assertThat(actual, equalTo(expected));
     }
 
     @Test
-    public void testx2() {
-        final String zoneId = TzWhere.getLatLogTimeZoneId(-90, 0);
-        System.err.println("zoneId = " + zoneId);
+    public void testCoastalTimeZonesNotSupportedYet() {
+        final double latitude  = 38.4514;
+        final double longitude = -75.049;
+        final String actual = TzWhere.getLatLogTimeZoneId(latitude, longitude);
+        final String expected = null;
+
+        assertThat(actual, equalTo(expected));
     }
 
     @Test
-    public void testx3() {
-        final String zoneId = TzWhere.getLatLogTimeZoneId(89.9, 0);
-        System.err.println("zoneId = " + zoneId);
+    public void testDelawareOffShorePerGoogle1() {
+        final double latitude  = 38.4514;
+        final double longitude = -75.048;
+        final String actual = TzWhere.getLatLogTimeZoneId(latitude, longitude);
+        final String expected = null;
+
+        assertThat(actual, equalTo(expected));
     }
 
     @Test
-    public void testx4() {
-        final String zoneId = TzWhere.getLatLogTimeZoneId(-89.9, 0);
-        System.err.println("zoneId = " + zoneId);
+    public void testDelawareOffShorePerGoogleMaps2() {
+        final double latitude  = 38.4514;
+        final double longitude = -75.049;
+        final String actual = TzWhere.getLatLogTimeZoneId(latitude, longitude);
+        final String expected = null;
+
+        assertThat(actual, equalTo(expected));
     }
 
+    @Test
+    @Ignore("until accuracy is checked")
+    public void testDelawareOnShorePerGoogleMaps1() {
+        final double latitude  = 38.4514;
+        final double longitude = -75.050;
+        final String actual = TzWhere.getLatLogTimeZoneId(latitude, longitude);
+        final String expected = "America/New_York";
+
+        assertThat(actual, equalTo(expected));
+    }
+
+    @Test
+    public void testDelawareOnShorePerGoogleMaps2() {
+        final double latitude  = 38.4514;
+        final double longitude = -75.060;
+        final String actual = TzWhere.getLatLogTimeZoneId(latitude, longitude);
+        final String expected = "America/New_York";
+
+        assertThat(actual, equalTo(expected));
+    }
 
 }
